cbc3b2f44231d349393da09b5ec1ed9bdef51c53
Tuan Nguyen <tanguyen1@fcps.edu>
2023-05-01 10:33:14 -0400

    Completed - NavBar

--- udemy/src/app/app.module.ts
+++ udemy/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavbarComponent
   ],
   imports: [
     BrowserModule,
